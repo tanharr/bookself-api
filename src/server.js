5b6bfc07fd787b682c63db70f4842e968940e0e6
tanharr <kharismawan71@gmail.com>
2023-05-31 11:47:38 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
